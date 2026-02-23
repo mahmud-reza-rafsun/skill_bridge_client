d273e73debbb4f6daee391759064c31d41677941
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2026-02-23 23:08:38 +0600

    fix cookie related problem

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
+
 export const authClient = createAuthClient({
-    baseURL: "https://skill-bridge-back-end.vercel.app/api/auth",
+    baseURL: typeof window !== "undefined" ? window.location.origin : "",
     fetchOptions: {
         credentials: "include",
     },
